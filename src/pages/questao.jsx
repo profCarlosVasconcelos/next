bb25d039caff0e73547837140f71616a8a255097
Prof Carlos <carlos.albuquerque@al.senai.br>
2023-11-13 11:24:09 -0300

    Código questão refetorado.

--- src/pages/questao.jsx
+++ src/pages/questao.jsx
@@ -4,20 +4,25 @@ export default function Questao() {
     const [questao, setQuestao] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:4077/api/questao/123456')
+        fetch('http://localhost:4077/api/questao/22')
             .then(resp => resp.json())
             .then(setQuestao)
 
     }, [])
+
+    function renderizarRespostas() {
+        if (questao) {
+            return questao.respostas.map(resp => <li key={resp.id}>{resp}</li>
+            )
+        }
+    }
+
     return (
         <div>
             <h1>Questão</h1>
-            <div><>
-                <span>{questao?.enunciado}</span>
-                <br />
-                <span>{questao?.respostas}</span>
-            </>
-
+            <div>
+                <span>{questao?.id} - {questao?.enunciado}</span>
+                {renderizarRespostas()}
             </div>
         </div>
     )
